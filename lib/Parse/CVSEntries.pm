874fa439c1f7bc83e75394c6bc685b5a53829236
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2003-06-25 11:53:01 +0000

    refactor down to one file
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Parse-CVSEntries@1312 c56e198b-7db7-0310-b3ab-8e406f684e33

--- lib/Parse/CVSEntries.pm
+++ lib/Parse/CVSEntries.pm
@@ -1,6 +1,6 @@
 use strict;
 package Parse::CVSEntries;
-use Parse::CVSEntry;
+our $VERSION = '0.01';
 
 =head1 NAME
 
@@ -12,6 +12,8 @@ Parse::CVSEntries - parse a CVS/Entries file
  for my $entry ($parsed->entries) {
      print $entry->name, " ", $entry->version, "\n";
  }
+
+=head1 DESCRIPTION
 
 =cut
 
@@ -31,5 +33,44 @@ sub entry_class {
     'Parse::CVSEntry';
 }
 
+
+package Parse::CVSEntry;
+use base 'Class::Accessor::Fast';
+use Date::Parse qw( str2time );
+
+# the actual fields in the file
+my @fields = qw( dir name version modified bar baz );
+__PACKAGE__->mk_accessors( @fields, 'mtime' );
+
+sub new {
+    my $class = shift;
+    my $line  = shift;
+
+    my %self;
+    @self{ @fields } = split /\//, $_;
+    $self{mtime} = str2time $self{modified}, "UTC"
+      unless $self{dir};
+
+    $class->SUPER::new(\%self);
+}
+
 1;
 __END__
+
+
+=head1 AUTHOR
+
+Richard Clamp <richardc@unixbeard.net>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2003 Richard Clamp.  All Rights Reserved.
+
+This module is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<File::Find::Rule::CVS>, cvs(1)
+
+=cut
